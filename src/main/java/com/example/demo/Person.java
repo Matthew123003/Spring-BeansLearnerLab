f417a7ba0f14f67c1ea66102b93c189053d41591
Matt Kramer <matthew123003@gmail.com>
2024-04-29 14:53:03 -0400

    Part 0,1,2 Done

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class Person {
+
+    private final Long id = 1L;
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
